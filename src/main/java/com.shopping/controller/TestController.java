1ac8edc5323037444acac58c9d643a53f1916ebb
henry <277630836@qq.com>
2018-11-13 01:32:24 +0800

    update

--- src/main/java/com.shopping/controller/TestController.java
+++ src/main/java/com.shopping/controller/TestController.java
@@ -2,6 +2,7 @@ package com.shopping.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shopping.domain.User;
+import com.shopping.dto.ResultEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/test")
@@ -18,9 +21,14 @@ public class TestController {
 
     @RequestMapping(value="/dep",method=RequestMethod.GET)
     @ResponseBody
-    public String selectUser(@RequestParam(required = false, value = "temp")String temp){
+    public List selectUser(@RequestParam(required = false, value = "temp")String temp){
         System.currentTimeMillis();
-        return "test2.jsp";
+        List<String> list = new ArrayList<String>();
+        list.add("/image/s1.png");
+        list.add("/image/s2.png");
+        list.add("/image/s3.png");
+//        return new ResultEntity<>("200","success","/image/s1.png");
+        return list;
     }
 
 }
